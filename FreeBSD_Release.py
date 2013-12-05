eb82821382d8c29253db11f356be7327eee4cccf
Shawn Webb <lattera@gmail.com>
2013-12-04 20:38:22 -0500

    Rely on the config file for branches. Support multiple source branches.

--- FreeBSD_Release.py
+++ FreeBSD_Release.py
@@ -12,30 +12,57 @@ class Release:
     def Describe(self):
         return "ISO/memstick"
 
+    def get_src_job(self, job):
+        for dep in job.dependencies:
+            if dep.name == "FreeBSD Source":
+                return dep
+
+        return None
+
     def Run(self, job, config):
         curdir = os.getcwd()
-        os.chdir("/usr/src")
+        newdir = "/usr/src"
+        srcjob = self.get_src_job(job)
+        if srcjob == None:
+            return False
+        if "srcdir" in srcjob.options:
+            newdir = srcjob.options["srcdir"]
+
+        if not "branches" in srcjob.options:
+            return True
+
+        os.chdir(newdir)
         
+        succeeded = True
         with job.GetLogfile(config) as logfile:
-            status = subprocess.call(["make", "-DNO_CLEAN", "-sj" + str(self.jobs), "KERNCONF=" + self.kernel, "buildworld", "buildkernel"], stdout=logfile, stderr=subprocess.STDOUT)
-            if status != 0:
-                os.chdir(curdir)
-                return False
+            for branch in srcjob.options["branches"]:
+                if not branch["succeeded"]:
+                    continue
 
-            os.chdir("/usr/src/release")
-            status = subprocess.call(["sudo", "make", "clean"], stdout=logfile, stderr=subprocess.STDOUT)
-            if status != 0:
-                os.chdir(curdir)
-                return False
+                if not srcjob.instance.checkout_branch(branch["local-branch"], logfile):
+                    succeeded = False
+                    continue
 
-            status = subprocess.call(["sudo", "make", "-s", "KERNCONF=" + self.kernel, "release"], stdout=logfile, stderr=subprocess.STDOUT)
-            if status != 0:
-                os.chdir(curdir)
-                return False
+                status = subprocess.call(["make", "-sj" + str(self.jobs), "KERNCONF=" + self.kernel, "buildworld", "buildkernel"], stdout=logfile, stderr=subprocess.STDOUT)
+                if status != 0:
+                    os.chdir(curdir)
+                    return False
 
-        for filename in os.listdir("/usr/obj/usr/src/release"):
-            if len(filename) > 3 and filename[-3:] in ["txz", "iso"]:
-                shutil.copy("/usr/obj/usr/src/release/" + filename, self.local_destdir)
+                os.chdir("/usr/src/release")
+                status = subprocess.call(["sudo", "make", "clean"], stdout=logfile, stderr=subprocess.STDOUT)
+                if status != 0:
+                    os.chdir(curdir)
+                    return False
+
+                status = subprocess.call(["sudo", "make", "-s", "KERNCONF=" + self.kernel, "release"], stdout=logfile, stderr=subprocess.STDOUT)
+                if status != 0:
+                    os.chdir(curdir)
+                    return False
+
+                if "destdir" in branch:
+                    for filename in os.listdir("/usr/obj/usr/src/release"):
+                        if len(filename) > 3 and filename[-3:] in ["txz", "iso"]:
+                            shutil.copy("/usr/obj/usr/src/release/" + filename, branch["destdir"])
 
         os.chdir(curdir)
         return True
--- FreeBSD_Source.py
+++ FreeBSD_Source.py
@@ -4,36 +4,58 @@ import subprocess
 
 class Source:
     def __init__(self):
-        self.dataset = "tank/src/freebsd"
-        self.upstream_remote = "upstream-github"
-        self.upstream_branch = "master"
-        self.local_branch = "soldierx/lattera/aslr"
-        self.commit_msg = "[automated] Merge in " + self.upstream_remote + "/" + self.upstream_branch
-        self.push_to = "origin"
+        pass
 
     def Describe(self):
         return "Source"
 
+    def fetch_upstream(self, upstream, logfile):
+        status = subprocess.call(["git", "fetch", upstream], stdout=logfile, stderr=subprocess.STDOUT)
+        return status == 0
+
+    def checkout_branch(self, branch, logfile):
+        status = subprocess.call(["git", "checkout", branch], stdout=logfile, stderr=subprocess.STDOUT)
+        return status == 0
+
+    def merge_upstream(self, upstream, upstream_branch, logfile):
+        status = subprocess.call(["git", "merge", "-m", "[automated] Merge in " + upstream + "/" + upstream_branch, upstream + "/" + upstream_branch], stdout=logfile, stderr=subprocess.STDOUT)
+        return status == 0
+
+    def push_remote(self, local_branch, remote, remote_branch, logfile):
+        status = subprocess.call(["git", "push", remote, local_branch + ":" + remote_branch], stdout=logfile, stderr=subprocess.STDOUT)
+        return status == 0
+
     def Run(self, job, config):
         curdir = os.getcwd()
-        newdir = subprocess.check_output(["zfs", "get", "-H", "-ovalue", "mountpoint", self.dataset]).strip()
+        newdir = "/usr/src"
+        if "srcdir" in job.options:
+            newdir = job.options["srcdir"]
+
         os.chdir(newdir)
 
         with job.GetLogfile(config) as logfile:
-            status = subprocess.call(["git", "fetch", self.upstream_remote], stdout=logfile, stderr=subprocess.STDOUT)
-            if status != 0:
-                os.chdir(curdir)
-                return False
+            for branch in job.options["branches"]:
+                branch["succeeded"] = False
+                if not self.fetch_upstream(branch["upstream"], logfile):
+                    continue
 
-            status = subprocess.call(["git", "merge", "-m", self.commit_msg, self.upstream_remote + "/" + self.upstream_branch], stdout=logfile, stderr=subprocess.STDOUT)
-            if status != 0:
-                os.chdir(curdir)
-                return False
+                if not self.checkout_branch(branch["local-branch"], logfile):
+                    continue
 
-            status = subprocess.call(["git", "push", self.push_to, self.local_branch], stdout=logfile, stderr=subprocess.STDOUT)
-            if status != 0:
-                os.chdir(curdir)
-                return False
+                if not self.merge_upstream(branch["upstream"], branch["upstream-branch"], logfile):
+                    continue
+
+                if "push-remote" in branch:
+                    if not self.push_remote(branch["local-branch"], branch["push-remote"], branch["push-remote-branch"], logfile):
+                        continue
+
+                branch["succeeded"] = True
 
             os.chdir(curdir)
-            return True
+            succeeded = True
+            for branch in job.options["branches"]:
+                if branch["succeeded"] == False:
+                    succeeded = False
+                    break
+
+            return succeeded
